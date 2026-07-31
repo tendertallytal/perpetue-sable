071bf3f94b2ff98fddfad3d371138236e3a9838d
Natalia Rai <nataliarai@MacBook-Pro-3.local>
2026-07-31 13:14:16 -0400

    change model

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: Request) {
     const { messages } = await request.json();
 
     const completion = await openai.chat.completions.create({
-      model: 'yourname/your-endpoint-name',
+      model: 'nataliarai2911-c3cd/PerpetueSable',
       messages,
       stream: false,
     });
